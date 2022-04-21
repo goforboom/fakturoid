2ac9cc2b348a8d95b6dbd3337ef371e8c9cb5a6e
Jiří Svěcený <jiri.sveceny@sitole.cz>
2022-04-21 10:23:15 +0200

    Release Fakturoid SDK to Gradle public registry

--- fakturoid/build.gradle.kts
+++ fakturoid/build.gradle.kts
@@ -3,11 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	kotlin("jvm") version "1.6.10"
 
+	// https://github.com/gradle-nexus/publish-plugin
+	id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 	id("maven-publish")
+	id("signing")
 }
 
 group = "com.goforboom"
-version = "0.0.1"
+version = "1.0.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
@@ -32,27 +35,77 @@ dependencies {
 	testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
 }
 
+java {
+	withSourcesJar()
+	withJavadocJar()
+}
+
+signing {
+	val signingKey: String? = System.getenv("GRADLE_GPG_SIGN_KEY")
+	val signingPassword: String? = System.getenv("GRADLE_GPG_SIGN_PASSWORD")
+
+	useInMemoryPgpKeys(signingKey, signingPassword)
+
+	sign(publishing.publications)
+}
+
+
 publishing {
 	publications {
-		create<MavenPublication>("kotlin") {
-			groupId = "com.goforboom"
-			artifactId = "fakturoid"
-			version = version
+		create<MavenPublication>("sdk") {
+			pom {
+				groupId = "com.goforboom"
+				artifactId = "fakturoid-sdk"
+				version = version
+
+				name.set("Fakturoid SDK")
+				description.set("Implementation of Fakturoid API for Java/Kotlin in tiny SDK")
+				url.set("https://github.com/goforboom/fakturoid")
+
+				licenses {
+					license {
+						name.set("The Apache License, Version 2.0")
+						url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+					}
+				}
+
+				developers {
+					developer {
+						id.set("goforboom")
+						name.set("BOOM")
+						email.set("dev@goforboom.com")
+					}
+				}
+
+				scm {
+					connection.set("scm:git:git://github.com:goforboom/fakturoid.git")
+					developerConnection.set("scm:git:ssh://github.com:goforboom/fakturoid.git")
+					url.set("https://github.com/goforboom/fakturoid")
+				}
+			}
 
 			from(components["java"])
 		}
 	}
+}
 
+nexusPublishing {
 	repositories {
-		maven {
-			name = "goforboom"
-			url = uri("https://maven.pkg.github.com/goforboom/fakturoid")
-			credentials {
+		create("sdk") {
+			nexusUrl.set(
+				uri("https://s01.oss.sonatype.org/service/local/")
+			)
 
-				// Credentials for GitHub package publish
-				username = System.getenv("GITHUB_USERNAME")
-				password = System.getenv("GITHUB_TOKEN")
-			}
+			snapshotRepositoryUrl.set(
+				uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+			)
+
+			username.set(
+				System.getenv("SONATYPE_USERNAME")
+			)
+			password.set(
+				System.getenv("SONATYPE_PASSWORD")
+			)
 		}
 	}
 }
